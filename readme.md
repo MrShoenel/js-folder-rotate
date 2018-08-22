1cac0a4bf4e55959c29f72bbfe58585fce7f403a
Sebastian Hönel <development@hoenel.net>
2018-08-22 19:41:40 +0200

    Commit of initial version with full implementation, 98% coverage, readme, typedefs as version v1.0.0

---
+++ readme.md
@@ -0,0 +1,5 @@
+# FolderRotate
+FolderRotate (or: `folder-rotate`) is a universal module for Node.js for rotating folders (restricting them to a certain size). The restrictions can be set per folder using certain rules.
+
+## Install via npm <span style="vertical-align:middle">[![Current Version](https://img.shields.io/npm/v/sh.folder-rotate.svg)](https://www.npmjs.com/package/sh.folder-rotate)</span>
+`npm install sh.folder-rotate`
---
+++ test/test_FolderRotate.js
@@ -0,0 +1,215 @@
+require('../meta/typedefs');
+
+
+const { assert } = require('chai')
+, path = require('path')
+, fs = require('fs')
+, fsX = require('fs-extra')
+, testFolder = path.resolve(path.join(__dirname, 'testFiles'))
+, { FolderRotate } = require('../lib/FolderRotate')
+, { assertThrowsAsync, timeout } = require('sh.orchestration-tools');
+
+
+/**
+ * @param {string} name 
+ * @param {number} size 
+ * @returns {Promise.<string>} the abs path
+ */
+const createFile = (name, size) => new Promise((res, rej) => {
+  try {
+    const buf = Buffer.alloc(size, 'a')
+    , n = path.resolve(path.join(testFolder, name))
+    , w = fs.createWriteStream(n);
+
+    w.write(buf, err => {
+      if (err) {
+        rej(err);
+      } else {
+        w.end(() => {
+          res(n);
+        });
+      }
+    });
+  } catch (e) {
+    rej();
+  }
+});
+
+const touchFile = absPath => new Promise((res, rej) => {
+  try {
+    fs.appendFileSync(absPath, 'a');
+    res();
+  } catch (e) {
+    rej(e);
+  }
+});
+
+
+describe('FolderRotate', function() {
+
+  this.beforeEach(() => {
+    if (fs.existsSync(testFolder)) {
+      try {
+        fsX.removeSync(testFolder);
+      } catch (e) { }
+    }
+    try {
+      fs.mkdirSync(testFolder);
+    } catch (e) { }
+  });
+
+  this.afterEach(() => {
+    try {
+      fsX.removeSync(testFolder);
+    } catch (e) { }
+  });
+
+  it('should throw if given invalid arguments', done => {
+    assert.doesNotThrow(() => {
+      FolderRotate.getByteSizeForRotationUnit('b');
+      FolderRotate.getByteSizeForRotationUnit('kb');
+      FolderRotate.getByteSizeForRotationUnit('mb');
+      FolderRotate.getByteSizeForRotationUnit('gb');
+      FolderRotate.getByteSizeForRotationUnit('tb');
+    });
+
+    assert.throws(() => {
+      FolderRotate.getByteSizeForRotationUnit('qty');
+    }, /The unit 'qty' is not a byte-size unit./i);
+
+    assert.throws(() => {
+      FolderRotate.getByteSizeForRotationUnit('f');
+    }, /The unit 'f' is not known/i);
+
+    done();
+  });
+
+  it('should throw for unknown sorting methods', async() => {
+    /** @type {RotateConfig} */
+    const conf = {
+      maxSize: 1,
+      orderBy: 'date_birthtime',
+      orderDir: 'asc',
+      path: testFolder,
+      unit: 'qty'
+    };
+
+    const fr = new FolderRotate(conf);
+    await createFile('ase', 1);
+    await createFile('asd', 1);
+
+    conf.orderBy = 'foo'; // INVALID!
+
+    await assertThrowsAsync(async() => {
+      await fr.rotate(); // would delete
+    });
+  });
+
+  it('should throw for invalid files or directories', async() => {
+    await assertThrowsAsync(async() => {
+      await FolderRotate.statFileAsync('invalid:///file.bla');
+    });
+
+    await assertThrowsAsync(async() => {
+      await FolderRotate.getFilesAsync('/invalid-dir/', f => true);
+    });
+  });
+
+  it('should be able to create files with certain size', async() => {
+    const n = await createFile(`test_${+new Date}`, 500);
+    assert.isTrue(fs.existsSync(n));
+    const s = fs.statSync(n);
+    assert.strictEqual(s.size, 500);
+    fs.unlinkSync(n);
+  });
+
+  it('should delete the right files if sorted by name or size', async() => {
+    /** @type {RotateConfig} */
+    const conf = {
+      maxSize: 1,
+      orderBy: 'name',
+      orderDir: 'asc',
+      path: testFolder,
+      unit: 'qty'
+    };
+
+    const fr = new FolderRotate(conf);
+
+    await createFile('ase', 90);
+    await createFile('asd', 100);
+
+    let f = await fr.rotate(true);
+    assert.strictEqual(f.length, 1);
+    assert.strictEqual(f[0].name, 'ase');
+    
+    conf.orderBy = 'size';
+    f = await fr.rotate(true);
+    assert.strictEqual(f.length, 1);
+    assert.strictEqual(f[0].name, 'asd');
+
+    // Now raise the qty:
+    conf.maxSize = 2;
+    f = await fr.rotate(true);
+    assert.strictEqual(f.length, 0);
+
+    conf.maxSize = 189;
+    conf.unit = 'b';
+    f = await fr.rotate(true);
+    assert.strictEqual(f.length, 1);
+    assert.strictEqual(f[0].name, 'asd');
+
+    conf.maxSize = 190;
+    f = await fr.rotate(true);
+    assert.strictEqual(f.length, 0);
+  });
+
+  it('should delete the right files if sorted by any of the times', async() => {
+    /** @type {RotateConfig} */
+    const conf = {
+      filesRegex: {
+        regex: '.*',
+        flags: ['i']
+      },
+      maxSize: 1,
+      orderBy: 'date_birthtime',
+      orderDir: 'asc',
+      path: testFolder,
+      unit: 'qty'
+    };
+    const fr = new FolderRotate(conf);
+
+    const ase = await createFile('ase', 1);
+    await timeout(50);
+    const asd = await createFile('asd', 1);
+
+    let f = await fr.rotate(true);
+    assert.strictEqual(f.length, 1);
+    assert.strictEqual(f[0].name, 'asd');
+
+    conf.orderBy = 'date_mtime';
+    f = await fr.rotate(true);
+    assert.strictEqual(f.length, 1);
+    assert.strictEqual(f[0].name, 'asd');
+
+    conf.orderBy = 'date_atime';
+    f = await fr.rotate(true);
+    assert.strictEqual(f.length, 1);
+    assert.strictEqual(f[0].name, 'asd');
+
+    conf.orderBy = 'date_ctime';
+    f = await fr.rotate(true);
+    assert.strictEqual(f.length, 1);
+    assert.strictEqual(f[0].name, 'asd');
+
+    await timeout(25);
+    await touchFile(ase);
+    f = await fr.rotate(true);
+    assert.strictEqual(f.length, 1);
+    assert.strictEqual(f[0].name, 'ase');
+
+    conf.orderBy = 'date_birthtime';
+    f = await fr.rotate(); // Will delete files
+    assert.strictEqual(f.length, 1);
+    assert.strictEqual(f[0].name, 'asd');
+  });
+});
